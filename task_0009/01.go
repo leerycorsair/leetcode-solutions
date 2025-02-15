ecaa120d11763419409297f0dea6e55f3c22cfef
Leonov Vladislav <vladleonov21032002@gmail.com>
2025-02-16 02:11:49 +0300

    task_0009

---
+++ task_0009/01.go
@@ -0,0 +1,15 @@
+package task0009
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+	x_str := fmt.Sprint(x)
+
+	for i := range len(x_str) / 2 {
+		if x_str[i] != x_str[len(x_str)-i-1] {
+			return false
+		}
+	}
+
+	return true
+}

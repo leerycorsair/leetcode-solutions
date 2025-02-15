8c42d4d7c20ef150369f69c5e774034ad667b775
Leonov Vladislav <vladleonov21032002@gmail.com>
2025-02-16 02:22:33 +0300

    task_0009

--- task_0009/02.go
+++ task_0009/02.go
@@ -2,9 +2,12 @@ package task0009
 
 import "fmt"
 
-func isPalindrome_01(x int) bool {
-	x_str := fmt.Sprint(x)
+func isPalindrome_02(x int) bool {
+	if x < 0 {
+		return false
+	}
 
+	x_str := fmt.Sprint(x)
 	for i := range len(x_str) / 2 {
 		if x_str[i] != x_str[len(x_str)-i-1] {
 			return false
